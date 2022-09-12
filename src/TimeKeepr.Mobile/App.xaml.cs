536fb904c082919c9a5929aa69c403b3d85d9677
eruvalca <eruvalca@outlook.com>
2022-09-12 12:58:26 -0400

    - added dotnet maui mobile projects - added shared razor class library project

---
+++ src/TimeKeepr.Mobile/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace TimeKeepr.Mobile
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
